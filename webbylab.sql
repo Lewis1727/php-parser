2832464ab4bdf974e214c37306550846dfafe2ad
Lewis1727 <59251780+Lewis1727@users.noreply.github.com>
2021-04-13 16:44:17 +0300

    Add files via upload

--- webbylab.sql
+++ webbylab.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 09, 2021 at 03:14 PM
--- Server version: 10.5.5-MariaDB
--- PHP Version: 7.4.11
+-- Generation Time: Apr 13, 2021 at 01:43 PM
+-- Server version: 10.5.9-MariaDB
+-- PHP Version: 8.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `films` (
   `id` int(200) NOT NULL,
   `name` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `year` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `year` year(4) NOT NULL,
   `format` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
   `actors` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -53,7 +53,7 @@ ALTER TABLE `films`
 -- AUTO_INCREMENT for table `films`
 --
 ALTER TABLE `films`
-  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=165;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
